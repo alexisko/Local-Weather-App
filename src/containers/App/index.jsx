34b3a70ac6f0ad5930af6417c57a7bc40628a4a5
Alexis Okamura <alexiskokamura@gmail.com>
2018-09-16 21:55:38 -0700

    Add fontawesome library

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 import WeatherApp from '../WeatherApp';
+
+library.add(faSearch);
 
 class App extends Component {
   render() {
